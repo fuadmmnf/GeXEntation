5759394fdf9770ee133434f00e5a8725b441bd84
fuadmmnf <bsse0920@iit.du.ac.bd>
2018-10-14 23:25:45 +0600

    2 gestures added

--- src/main_file.py
+++ src/main_file.py
@@ -12,6 +12,9 @@ import pyautogui
 # from thumbdown import t_crop
 # from okay import ok_crop
 # from direction import dir_crop
+#from finger_count import fin_count
+
+
 
 #os.chdir("/home/gesentation/src/haarCascades")
 right_h1_cascade=cv2.CascadeClassifier('./haarCascades/rpalm.xml') # right palm
@@ -20,13 +23,13 @@ right_h1_cascade=cv2.CascadeClassifier('./haarCascades/rpalm.xml') # right palm
 #right_cascade=cv2.CascadeClassifier('right.xml') #right direction
 #left_cascade=cv2.CascadeClassifier('left.xml') #left direction
 #finger_count_cascade = cv2.CascadeClassifier('finger_count.xml')
-#okay_cascade = cv2.CascadeClassifier('ok.xml') # okay sign
-#point_cascade = cv2.CascadeClassifier('point1.xml')
+#okay_cascade = cv2.CascadeClassifier('./haarCascades/ok.xml') # okay sign
+point_cascade = cv2.CascadeClassifier('./haarCascades/point1.xml')
 #fin_cascade=cv2.CascadeClassifier('fin_2.xml') 
-hand_left_to_right_cascade = cv2.CascadeClassifier('./haarCascades/heppu_handb.xml')
-hand_right_to_left_cascade = cv2.CascadeClassifier('./haarCascades/heppu_hands.xml')
-#fist_cascade=cv2.CascadeClassifier('aGest.xml') # fist
-#thumbdown_cascade = cv2.CascadeClassifier('thumbdown.xml')
+#hand_left_to_right_cascade = cv2.CascadeClassifier('./haarCascades/heppu_handb.xml')
+#hand_right_to_left_cascade = cv2.CascadeClassifier('./haarCascades/heppu_hands.xml')
+fist_cascade=cv2.CascadeClassifier('./haarCascades/fist.xml') # fist
+thumbdown_cascade = cv2.CascadeClassifier('./haarCascades/thumbdown.xml')
 cap = cv2.VideoCapture(0)
 ca=0
 start_time = -1 
@@ -46,19 +49,19 @@ while(1):
         if(start_time==-1 or finish_time-start_time>=0.3):
         
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                #point=point_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
+                point=point_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
                 #fin=fin_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,flags=0, minSize=(100,80))
                 right_palm = right_h1_cascade.detectMultiScale(gray,1.1, 5)
                 #hand = hand_cascade.detectMultiScale(gray,1.1, 5)
-                LtoR = hand_left_to_right_cascade.detectMultiScale(gray, 1.1, 5)
-                RtoL = hand_right_to_left_cascade.detectMultiScale(gray, 1.1, 5)
+                #LtoR = hand_left_to_right_cascade.detectMultiScale(gray, 1.1, 5)
+                #RtoL = hand_right_to_left_cascade.detectMultiScale(gray, 1.1, 5)
                 #finger_count = finger_count_cascade.detectMultiScale(gray,1.1,5)
                 #left_palm = left_h1_cascade.detectMultiScale(gray,1.1, 5)
                 #right_dir = right_cascade.detectMultiScale(gray,1.1, 5)
                 #left_dir = left_cascade.detectMultiScale(gray,1.1, 5)
-                #fist=fist_cascade.detectMultiScale(gray,1.3, 5)
+                fist=fist_cascade.detectMultiScale(gray,1.3, 5)
                 #okay=okay_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,150))
-                #thumbdown=thumbdown_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
+                thumbdown=thumbdown_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3,flags=0, minSize=(100,80))
 
                 #print(len(finger_count)/4)
                 # for (x,y,w,h) in left_palm:
@@ -72,15 +75,15 @@ while(1):
 
 
 
-                # for (x,y,w,h) in fist:
-                #         #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                #         # roi_gray = gray[y:y+h,x:x+w]
-                #         # roi_color = img[y:y+h,x:x+w]
-                #         # crop_img = img[y:y+h,x:x+w]
-                #         # fs_crop(crop_img,img)
-                #         isValid = True
-                #         print('fist')
-                #         start_time = time.time()
+                for (x,y,w,h) in fist:
+                        #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                        # roi_gray = gray[y:y+h,x:x+w]
+                        # roi_color = img[y:y+h,x:x+w]
+                        # crop_img = img[y:y+h,x:x+w]
+                        # fs_crop(crop_img,img)
+                        isValid = True
+                        print('fist')
+                        start_time = time.time()
                        
 
                 # for (x,y,w,h) in hand:
@@ -105,25 +108,25 @@ while(1):
 
                
                 # print('')
-                if isValid:
-                        # for (x,y,w,h) in RtoL:
-                        #         #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                        #         # roi_gray=gray[y:y+h,x:x+w]
-                        #         # roi_color=img[y:y+h,x:x+w]
-                        #         # crop_img=img[y:y+h,x:x+w]
-                        #         # h_crop(crop_img,img, 'right')
-                        #         print('right swipe')
-                        #         start_time = time.time()
+                # if isValid:
+                #         # for (x,y,w,h) in RtoL:
+                #         #         #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                #         #         # roi_gray=gray[y:y+h,x:x+w]
+                #         #         # roi_color=img[y:y+h,x:x+w]
+                #         #         # crop_img=img[y:y+h,x:x+w]
+                #         #         # h_crop(crop_img,img, 'right')
+                #         #         print('right swipe')
+                #         #         start_time = time.time()
 
-                        for (x,y,w,h) in LtoR:
-                                #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                                # roi_gray=gray[y:y+h,x:x+w]
-                                # roi_color=img[y:y+h,x:x+w]
-                                # crop_img=img[y:y+h,x:x+w]
-                                # h_crop(crop_img,img, 'right')
-                                #if isValid:
-                                print('left swipe')
-                                start_time = time.time()
+                #         for (x,y,w,h) in LtoR:
+                #                 #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+                #                 # roi_gray=gray[y:y+h,x:x+w]
+                #                 # roi_color=img[y:y+h,x:x+w]
+                #                 # crop_img=img[y:y+h,x:x+w]
+                #                 # h_crop(crop_img,img, 'right')
+                #                 #if isValid:
+                #                 print('left swipe')
+                #                 start_time = time.time()
 
 
                 # for (x,y,w,h) in right_dir:
@@ -163,34 +166,18 @@ while(1):
 
 
                 # for (x,y,w,h) in okay:
-                #         #cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-                #         roi_gray=gray[y:y+h,x:x+w]
-                #         roi_color=img[y:y+h,x:x+w]
-                #         crop_img=img[y:y+h,x:x+w]
-                #         ok_crop(crop_img,img)
+                #         print('okay')
                 #         start_time = time.time()
-                #         #time.sleep(0.5)
+                       
                         
-                        
-                # for (x,y,w,h) in point:
-                #         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,255),2)
-                #         roi_gray=gray[y:y+h,x:x+w]
-                #         roi_color=img[y:y+h,x:x+w]
-                #         crop_img=img[y:y+h,x:x+w]
-                #         ha=2
-                #         p_crop(crop_img,img)
-                #         #time.sleep(0.5)
-
+                for (x,y,w,h) in point:
+                        print('point')
+                        start_time = time.time()
                 
 
-                # for (x,y,w,h) in thumbdown:
-                #         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                #         roi_gray=gray[y:y+h,x:x+w]
-                #         roi_color=img[y:y+h,x:x+w]
-                #         crop_img=img[y:y+h,x:x+w]
-                #         ha=5
-                #         t_crop(crop_img,img)
-                #         #time.sleep(0.5)
+                for (x,y,w,h) in thumbdown:                    
+                        print('thumbs down')
+                        start_time = time.time()
 
                 
 
